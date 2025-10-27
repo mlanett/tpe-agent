4fb78badea9ef85a1992c58e1c344ad0e0a6a9fc
Mark Lanett <mark.lanett@gmail.com>
2025-10-27 12:44:06 -0700

    Fix: tests not running.

--- monitoring-agent/build.gradle.kts
+++ monitoring-agent/build.gradle.kts
@@ -11,10 +11,12 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.1")
 }
 
 tasks.test {
     // Enable dynamic agent loading for tests
+    useJUnitPlatform()
     jvmArgs(
         "-XX:+EnableDynamicAgentLoading",
         "-Djdk.attach.allowAttachSelf=true"
